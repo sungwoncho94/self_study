4a50088a719929aba4597d01f01fc5dddd87be11
조성원 <sungwoncho94@naver.com>
2019-12-17 23:51:52 +0900

    사용자 이름 입력받기 / 3.4완료 / preventDefault()해결

--- 2_JS/greeting.js
+++ 2_JS/greeting.js
@@ -5,6 +5,10 @@ const greeting = document.querySelector(".js-greetings")
 const USER_LOCALSTRORAGE = "currentUser"
 const SHOWING_ON = "showing"
 
+// localstorage에 text를 저장한다
+function saveName(text) {
+    localStorage.setItem(USER_LOCALSTRORAGE, text)
+}
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -12,12 +16,14 @@ function handleSubmit(event) {
     // -> 저절로 발생되는 event를 막았기 때문
     const currentValue = input.value
     console.log(currentValue)
+    paintGreeting(currentValue)
+    saveName(currentValue)
 }
 
 function askName() {
     form.classList.add(SHOWING_ON)
-    // submit event가 발생하면 계속 위로 올라가서 document까지 올라감 (eventDefault)
     form.addEventListener("submit", handleSubmit)
+    // submit event가 발생하면 계속 위로 올라가서 document까지 올라감 (eventDefault)
 }
 
 function paintGreeting(text) {
@@ -36,7 +42,7 @@ function loadName() {
 }
 
 function init() {
-
+    loadName()
 }
 
 init()
